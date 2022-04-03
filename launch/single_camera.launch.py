2348c390ade4c92538b04ba3ec26c9c529ccf068
MasayaKataoka <ms.kataoka@gmail.com>
2022-04-03 15:34:45 +0900

    remove unused import
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- launch/single_camera.launch.py
+++ launch/single_camera.launch.py
@@ -15,19 +15,8 @@
 from ipaddress import ip_address
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.conditions import IfCondition
-from launch.event_handlers import OnProcessExit
-from launch.events import Shutdown
-from launch.actions import (
-    EmitEvent,
-    ExecuteProcess,
-    IncludeLaunchDescription,
-    RegisterEventHandler,
-)
 from launch.actions.declare_launch_argument import DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import ComposableNodeContainer, Node
-from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
 from launch.substitutions.launch_configuration import LaunchConfiguration
 
 
